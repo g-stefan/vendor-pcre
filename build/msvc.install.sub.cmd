bb2598e76c5a3be0844cf04305e29582b1d295ea
Grigore Stefan <g_stefan@yahoo.com>
2021-11-29 14:47:43 +0200

    Update

--- build/msvc.install.sub.cmd
+++ build/msvc.install.sub.cmd
@@ -11,8 +11,8 @@ if errorlevel 1 exit 1
 rem ---
 
 if not exist "%INSTALL_PATH_BIN%\" mkdir "%INSTALL_PATH_BIN%"
-xcopy /Y /S /E "temp\pcre\bin\*.exe" "%INSTALL_PATH_BIN%\"
-xcopy /Y /S /E "temp\pcre\bin\*.dll" "%INSTALL_PATH_BIN%\"
+xcopy /Y /S /E "output\bin\*.exe" "%INSTALL_PATH_BIN%\"
+xcopy /Y /S /E "output\bin\*.dll" "%INSTALL_PATH_BIN%\"
 
 
 rem --- dev
@@ -21,7 +21,7 @@ if not exist "%INSTALL_PATH_DEV%\" mkdir "%INSTALL_PATH_DEV%"
 if not exist "%INSTALL_PATH_DEV%\bin" mkdir "%INSTALL_PATH_DEV%\bin"
 if not exist "%INSTALL_PATH_DEV%\include" mkdir "%INSTALL_PATH_DEV%\include"
 if not exist "%INSTALL_PATH_DEV%\lib" mkdir "%INSTALL_PATH_DEV%\lib"
-xcopy /Y /S /E "temp\pcre\bin\*.exe" "%INSTALL_PATH_DEV%\bin\"
-xcopy /Y /S /E "temp\pcre\bin\*.dll" "%INSTALL_PATH_DEV%\bin\"
-xcopy /Y /S /E "temp\pcre\include\" "%INSTALL_PATH_DEV%\include\"
-copy /Y /B "temp\pcre\lib\*.lib" "%INSTALL_PATH_DEV%\lib\*"
+xcopy /Y /S /E "output\bin\*.exe" "%INSTALL_PATH_DEV%\bin\"
+xcopy /Y /S /E "output\bin\*.dll" "%INSTALL_PATH_DEV%\bin\"
+xcopy /Y /S /E "output\include\" "%INSTALL_PATH_DEV%\include\"
+copy /Y /B "output\lib\*.lib" "%INSTALL_PATH_DEV%\lib\*"
--- build/msvc.make.cmd
+++ build/msvc.make.cmd
@@ -22,11 +22,13 @@ if not "%ACTION%" == "make" goto :eof
 
 call :cmdX xyo-cc --mode=%ACTION% --source-has-archive pcre
 
+if not exist output\ mkdir output
 if not exist temp\ mkdir temp
 
 set INCLUDE=%XYO_PATH_REPOSITORY%\include;%INCLUDE%
 set LIB=%XYO_PATH_REPOSITORY%\lib;%LIB%
 set WORKSPACE_PATH=%CD%
+set WORKSPACE_PATH_OUTPUT=%WORKSPACE_PATH%\output
 set WORKSPACE_PATH_BUILD=%WORKSPACE_PATH%\temp
 
 if exist %WORKSPACE_PATH_BUILD%\build.done.flag goto :eof
@@ -41,7 +43,7 @@ SET CMD_CONFIG=cmake
 SET CMD_CONFIG=%CMD_CONFIG% ../../source
 SET CMD_CONFIG=%CMD_CONFIG% -G "Ninja"
 SET CMD_CONFIG=%CMD_CONFIG% -DCMAKE_BUILD_TYPE=Release
-SET CMD_CONFIG=%CMD_CONFIG% -DCMAKE_INSTALL_PREFIX=%WORKSPACE_PATH_BUILD%\pcre
+SET CMD_CONFIG=%CMD_CONFIG% -DCMAKE_INSTALL_PREFIX=%WORKSPACE_PATH_OUTPUT%
 SET CMD_CONFIG=%CMD_CONFIG% -DBUILD_SHARED_LIBS=ON
 
 if not exist %WORKSPACE_PATH_BUILD%\build.configured.flag %CMD_CONFIG%
@@ -55,7 +57,7 @@ if errorlevel 1 goto makeError
 ninja clean
 if errorlevel 1 goto makeError
 
-del /Q /F ..\pcre\bin\*test.exe
+del /Q /F ..\..\output\bin\*test.exe
 
 goto buildDone
 
